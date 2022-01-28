e703f27cac549e8846ec56c5eaff481c9307d8c3
deep1617 <tarsem.1617@gmail.com>
2022-01-28 23:50:20 +0530

    completed appd task 1

---
+++ app/src/main/java/com/example/deepappd/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.deepappd
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val txt:TextView=findViewById(R.id.name)
+        val roll:TextView=findViewById(R.id.roll)
+        val btn: Button =findViewById(R.id.button)
+        btn.setOnClickListener {
+            val str1: String = txt.text.toString()
+            val str2: String = roll.text.toString()
+
+
+            if (str1.equals("", ignoreCase = true) ) {
+                Toast.makeText(this, "Please Enter Name", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "Hey $str1, Go Corona Go", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
